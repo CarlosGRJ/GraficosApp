4044b24fa0fc89b71ef07a8a94a8602cee4c8fdb
Carlos <carlosgrj2013@gmail.com>
2020-08-11 09:59:25 -0500

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { APP_ROUTES } from './app.routes';
+
+// Gráficos
+
+
 
 import { AppComponent } from './app.component';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
+
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent,
+    BarraComponent,
+    DonaComponent,
+    RadarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ChartsModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
+
+const RUTAS: Routes = [
+    { path: 'linea', component: LineaComponent },
+    { path: 'barra', component: BarraComponent },
+    { path: 'dona', component: DonaComponent },
+    { path: 'radar', component: RadarComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'linea' }
+];
+
+export const APP_ROUTES = RouterModule.forRoot( RUTAS );
